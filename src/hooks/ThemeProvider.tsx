d3e585ddedf21ab9b0d5797ce5d8edc741dc2dc2
haseebmohsin <haseeb11634@gmail.com>
2024-07-24 17:36:51 +0500

    context api theme...

---
+++ src/hooks/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+type ThemeContextType = {
+  theme: string;
+  toggleTheme: (mode: string) => void;
+};
+
+export const ThemeContext = createContext<ThemeContextType | null>(null);
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState("light");
+
+  function toggleTheme(mode: string) {
+    setTheme(mode);
+  }
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
---
+++ src/hooks/constant.ts
@@ -0,0 +1,4 @@
+export const theme = {
+  light: "#f8fafc",
+  dark: "#0f172a",
+};
