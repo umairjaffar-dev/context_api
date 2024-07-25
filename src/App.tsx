9c1a8ffae6dae3806a06ed2b02a771ff8b95eb2a
haseebmohsin <haseeb11634@gmail.com>
2024-07-25 16:18:42 +0500

    Home page setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
-import { twMerge } from "tailwind-merge";
-import { useContext } from "react";
-import { ThemeContext } from "./hooks/ThemeProvider";
+import Home from "./pages/Home";
+
 const App = () => {
-  const themeContext = useContext(ThemeContext);
-
-  const handleToggleTheme = () => {
-    themeContext?.toggleTheme(
-      themeContext?.theme === "light" ? "dark" : "light"
-    );
-    // console.log("theme", themeContext?.theme);
-  };
-
-  const darkTheme = themeContext?.theme === "dark" && "bg-gray-900";
   return (
-    <div
-      className={twMerge(
-        "w-screen h-screen grid grid-rows-[auto_1fr] gap-2 bg-gray-100",
-        darkTheme
-      )}
-    >
-      <div className="w-full h-16 px-10 border-b border-b-gray-500 flex justify-between items-center">
-        <p className="text-md font-semibold text-violet-500">LoGo</p>
-        <button
-          onClick={handleToggleTheme}
-          className={twMerge(
-            "text-sm text-violet-500 cursor-pointer bg-gray-900 rounded-md px-2 py-1 capitalize",
-            darkTheme
-          )}
-        >
-          mode
-        </button>
-      </div>
+    <div>
+      <Home />
     </div>
   );
 };
